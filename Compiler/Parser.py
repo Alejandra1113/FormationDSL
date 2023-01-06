f643b22f60126f86e09b6c5aa06bdcf2c58c67a1
Alejandra <amonzon1113@gmail,com>
2023-01-06 17:41:54 -0500

    Ast building

--- Compiler/Parser.py
+++ Compiler/Parser.py
@@ -21,7 +21,7 @@ class ShiftReduceParser:
         stack = [ 0 ]
         cursor = 0
         output = []
-        
+        ast = []
         while True:
             print(output)
             state = stack[-1]
@@ -94,7 +94,39 @@ class LR1Parser(ShiftReduceParser):
     def _register(table, key, value):
         assert key not in table or table[key] == value, 'Shift-Reduce or Reduce-Reduce conflict!!!'
         table[key] = value
+  
+  
+def evaluate_reverse_parse(right_parse, operations, tokens):
+    if not right_parse or not operations or not tokens:
+        return
+
+    right_parse = iter(right_parse)
+    tokens = iter(tokens)
+    stack = []
+    for operation in operations:
+        if operation == ShiftReduceParser.SHIFT:
+            token = next(tokens)
+            stack.append(token.lex)
         
+        elif operation == ShiftReduceParser.REDUCE:
+            production = next(right_parse)
+            head, body = production
+            attributes = production.attributes
+            assert all(rule is None for rule in attributes[1:]), 'There must be only synteticed attributes.'
+            rule = attributes[0]
+
+            if len(body):
+                synteticed = [None] + stack[-len(body):]
+                value = rule(None, synteticed)
+                stack[-len(body):] = [value]
+            else:
+                stack.append(rule(None, None))
+        else:
+            raise Exception('Invalid action!!!')
+
+    assert len(stack) == 1
+    assert isinstance(next(tokens).token_type, EOF)
+    return stack[0]      
 
 ######################## Visualize DataTable #############################       
 
