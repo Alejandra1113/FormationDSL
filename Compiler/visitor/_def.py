60bf55eba3c51e0c3cf04aba1e0b3a60bcc0d8c7
daaku <tonycadahia@gmail.com>
2023-01-09 18:29:20 -0500

    add_visitors

---
+++ Compiler/visitor/_def.py
@@ -0,0 +1,81 @@
+
+
+__all__ = ['Context', 'ProgramContext', 'OtherContext']
+
+
+class VariableInfo:
+    def __init__(self, name):
+        self.name = name
+
+
+class FunctionInfo:
+    def __init__(self, name, params):
+        self.name = name
+        self.params = params
+
+
+class Context:
+    def __init__(self, parent=None):
+        self.parent: Context = parent
+        self.children: dict[int, OtherContext] = []
+
+    def create_child_context(self, index):
+        child_scope = OtherContext(self)
+        self.children[index] = child_scope
+        return child_scope
+
+    def get_context(self, index):
+        return self.children.get(index) or self
+
+
+class ProgramContext(Context):
+    def __init__(self):
+        Context.__init__(self)
+        self.local_funcs: list[FunctionInfo] = []
+
+    def define_function(self, fname, params):
+        if not self.is_local_func(fname, len(params)):
+            func = FunctionInfo(fname, params)
+            self.local_funcs.append(func)
+            return func
+
+    def is_func_defined(self, fname, params):
+        return self.is_local_func(fname, params)
+
+    def is_local_func(self, fname, params):
+        return self.get_local_function_info(fname, params) is not None
+
+    def get_local_function_info(self, fname, params):
+        for info in self.local_funcs:
+            if info.name == fname and len(info.params) == params:
+                return info
+        return None
+
+
+class OtherContext(Context):
+    def __init__(self, parent=None):
+        self.parent: OtherContext
+        Context.__init__(self, parent)
+        self.local_vars: list[VariableInfo] = []
+
+    def is_func_defined(self, fname, params):
+        return self.parent.is_func_defined(fname, params)
+
+    def define_variable(self, vname):
+        if not self.is_var_defined(vname):
+            var = VariableInfo(vname)
+            self.local_vars.append(var)
+
+    def is_var_defined(self, vname):
+        if type(self) == ProgramContext or not self.is_local_var(vname):
+            return False
+        return self.parent.is_local_var(vname)
+
+    def is_local_var(self, vname):
+        return self.get_local_variable_info(vname) is not None
+
+    def get_local_variable_info(self, vname):
+        for i in self.local_vars:
+            if i.name == vname:
+                return i
+        return None
